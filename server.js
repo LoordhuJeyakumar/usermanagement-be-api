09e3fc1b38fd17c214aef31076e4f8be3a45860b
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2023-11-27 18:02:39 +0530

    cors removed

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
-const cors = require("cors");
+
 
 const PORT = process.env.PORT || 7000;
 server.use(middlewares);
 server.use(router);
-server.use(cors());
+
 server.listen(PORT, () => {
   console.log(`JSON Server is running ${PORT}`);
 });
