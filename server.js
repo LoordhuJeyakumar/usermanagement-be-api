f71831455f1edbd185cc21fb9eee0e8f48ba8555
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2023-11-27 17:56:57 +0530

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
-const PORT = process.env.PORT || 7000
-server.use(middlewares)
-server.use(router)
+const PORT = process.env.PORT || 7000;
+server.use(middlewares);
+server.use(router);
+server.use(cors());
 server.listen(PORT, () => {
-  console.log(`JSON Server is running ${PORT}`)
-})
+  console.log(`JSON Server is running ${PORT}`);
+});
